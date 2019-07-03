e384ded02efd4d273f0112762f03c99e9490a82c
yaoxunxun <xunxunyao@gmail.com>
2019-07-04 01:45:12 +0800

    featfix: 添加模块列表

--- bundler/runtime.js
+++ bundler/runtime.js
@@ -21,4 +21,21 @@
   //   [mododuleId]: codeFunction
   // }
   // TODO insert module here
+  {
+    0: (function(__require__, exports) {
+      let X = __require__(1)
+      const helloWorld = X.default('hello world!')
+      const node = document.createElement("div")
+      node.innerHTML = helloWorld + 'loading...'
+      // import(/* webpackChunkName: "async" */ './lazy').then(({ default: lazy }) => {
+      //   node.innerHTML = helloWorld + lazy
+      // })
+      document.body.appendChild(node)
+    }),
+    1: (function(__require__, exports) {
+      exports.default = (val) => {
+        return val && val.toUpperCase()
+      }
+    })
+  }
 ])
